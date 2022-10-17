78c45e12e4940bc7714e2a62eaca926ef23f2cd2
SheikhFarid99 <sheikhfarid498@gmail.com>
2022-10-17 23:48:24 +0600

    backend server create and run also frontend server run

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
+const dotenv = require('dotenv')
+dotenv.config({
+    path: 'config/config.env'
+})
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}!`))
